b667017100e65cccced3b5e9021ea1419d6ba862
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2018-04-10 20:17:49 -0300

    Add solution to XML Deserialization.

--- UdemyCSharp/SerializeDeserialize/AA_Library/User.cs
+++ UdemyCSharp/SerializeDeserialize/AA_Library/User.cs
@@ -3,5 +3,9 @@
         public string Name { get; set; }
         public string Id { get; set; }
         public string Email { get; set; }
+
+        public override string ToString() {
+            return $"Read: Name: {Name}, E-mail: {Email}, Id: {Id}";
+        }
     }
 }
---
+++ UdemyCSharp/SerializeDeserialize/AC_DeserializeXML/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using AA_Library;
+
+namespace AC_DeserializeXML {
+    internal class Program {
+        public static void Main(string[] args) {
+            var serializer = new XmlSerializer(typeof(User));
+
+            try {
+                var reader = new StreamReader("output.xml");
+                var user = (User) serializer.Deserialize(reader);
+                Console.Write($"{user.ToString()}\n");
+            } catch (Exception e) {
+                Console.Write($"Error: {e.Message}\n");
+            }
+
+            Console.Write("Finished deserialization.\n");
+        }
+    }
+}
